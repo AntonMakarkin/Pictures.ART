ed97848e649a6daa8c13d6cb713c7fa21ba1b4ad
AntonMakarkin <amakarkin27@gmail.com>
2021-02-07 23:33:00 +0300

    Lesson 23. Have finished the task

--- dist/script.js
+++ dist/script.js
@@ -4455,13 +4455,29 @@ __webpack_require__.r(__webpack_exports__);
 
 var accordion = function accordion(triggersSelector) {
   //check main.css on 921
-  var btns = document.querySelectorAll(triggersSelector);
-  btns.forEach(function (btn) {
+  var btns = document.querySelectorAll(triggersSelector),
+      blocksSelector = "." + btns[1].nextElementSibling.classList.item(2),
+      blocks = document.querySelectorAll(blocksSelector);
+  console.log(blocks);
+  btns.forEach(function (btn, i) {
     btn.addEventListener('click', function () {
       this.classList.toggle('active-style');
       this.nextElementSibling.classList.toggle('active-content');
 
       if (this.classList.contains('active-style')) {
+        btns.forEach(function (btn) {
+          //make all buttons inactive
+          btn.classList.remove('active-style');
+        });
+        this.classList.add('active-style'); //except this button where was a click
+
+        blocks.forEach(function (block) {
+          //make all textblocks hidden
+          block.classList.remove('active-content');
+          block.style.maxHeight = '0px';
+        });
+        this.nextElementSibling.classList.add('active-content'); //except which we want to show
+
         this.nextElementSibling.style.maxHeight = this.nextElementSibling.scrollHeight + 80 + "px";
       } else {
         this.nextElementSibling.style.maxHeight = '0px';
--- src/js/modules/accordion.js
+++ src/js/modules/accordion.js
@@ -1,12 +1,24 @@
 const accordion = (triggersSelector) => { //check main.css on 921
-    const btns = document.querySelectorAll(triggersSelector);
+    const btns = document.querySelectorAll(triggersSelector),
+          blocksSelector = "." + btns[1].nextElementSibling.classList.item(2),
+          blocks = document.querySelectorAll(blocksSelector);
+          console.log(blocks);
 
-    btns.forEach(btn => {
+    btns.forEach((btn, i) => {
         btn.addEventListener('click', function() {
             this.classList.toggle('active-style');
             this.nextElementSibling.classList.toggle('active-content');
 
             if (this.classList.contains('active-style')) {
+                btns.forEach(btn => { //make all buttons inactive
+                    btn.classList.remove('active-style');
+                });
+                this.classList.add('active-style'); //except this button where was a click
+                blocks.forEach(block => { //make all textblocks hidden
+                    block.classList.remove('active-content');
+                    block.style.maxHeight = '0px';
+                });
+                this.nextElementSibling.classList.add('active-content'); //except which we want to show
                 this.nextElementSibling.style.maxHeight = this.nextElementSibling.scrollHeight + 80 + "px";
             } else {
                 this.nextElementSibling.style.maxHeight = '0px';
